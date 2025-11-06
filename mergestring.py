a7b08d7d89d1f509a7f9937840b90359b8ed3ca6
dungngan <dungngan.work@gmail.com>
2025-11-06 17:16:04 +0700

    first leetcode

---
+++ mergestring.py
@@ -0,0 +1,26 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result =""
+        len1, len2 = len(word1), len(word2)
+        i = 0
+        j = 0      
+        while i < len1 or j < len2:
+            if i < len1:
+                result += word1[i]
+                i += 1
+            if j < len2:
+                result += word2[j]
+                j += 1
+        return result
+s1 = input("Enter first string: ")
+s2 = input("Enter second string: ")
+
+# 2. Tạo instance
+solution_instance = Solution()
+
+# 3. Gọi hàm với input đã lấy
+merged_string = solution_instance.mergeAlternately(s1, s2)
+
+# 4. In kết quả trả về
+print("Kết quả đã trộn:", merged_string)   
+        
